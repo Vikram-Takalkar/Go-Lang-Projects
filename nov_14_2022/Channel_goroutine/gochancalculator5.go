f38544c9ec5efc8b5e7b5c7fd4848c36c5e4321d
vikram <vikramtakalkar7@gmail.com>
2022-11-14 14:20:24 +0530

    my first commit

---
+++ nov_14_2022/Channel_goroutine/gochancalculator5.go
@@ -0,0 +1,37 @@
+package main
+
+import (  
+    "fmt"
+)
+
+func calcSquares(number int, squareop chan int) {  
+    sum := 0
+    for number != 0 {
+        fmt.Println("In calcSquares")
+        digit := number % 10
+        sum += digit * digit
+        number /= 10
+    }
+    squareop <- sum
+}
+
+func calcCubes(number int, cubeop chan int) {  
+    sum := 0 
+    for number != 0 {
+        fmt.Println("In calcCubes")
+        digit := number % 10
+        sum += digit * digit * digit
+        number /= 10
+    }
+    cubeop <- sum
+} 
+
+func main() {  
+    number := 589
+    sqrch := make(chan int)
+    cubech := make(chan int)
+    go calcSquares(number, sqrch)
+    go calcCubes(number, cubech)
+    squares, cubes := <-sqrch, <-cubech
+    fmt.Println("Final output", squares + cubes)
+}
